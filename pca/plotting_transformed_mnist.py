d73f209b811ae55688de076136c05d48ae018649
mahesh-keswani <karthikkeswani1234@gmail.com>
2021-02-06 23:08:03 +0530

    previous was just visualizing eigenvectors, in this visualizing when multiplied them with singular values

--- pca/plotting_transformed_mnist.py
+++ pca/plotting_transformed_mnist.py
@@ -17,10 +17,20 @@ def main():
 	plt.imshow(X[random_number].reshape( (28, 28) ), cmap='gray')
 	plt.show()
 
-	# taking top 196 dimensions and reshaping them as 14x14
+	# taking top 196 dimensions ( eigenvectors ) and reshaping them as 14x14
 	plt.imshow(random_image[:196].reshape( (14, 14) ), cmap='gray')
 	plt.show()
 
+	print(pca.singular_values_.shape)
+	print(pca.singular_values_)
+	# taking top 196 dimensions and multiplying with singular values
+	 # and reshaping them as 14x14
+
+	top_n_dim = 196
+	singular_values = pca.singular_values_[:top_n_dim]
+	plt.imshow((singular_values * random_image[:top_n_dim]).reshape( (int(np.sqrt(top_n_dim)), int(np.sqrt(top_n_dim))) ), cmap='gray')
+	plt.show()
+	
 
 if __name__ == '__main__':
     main()
